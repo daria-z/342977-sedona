fdbcd6260f1cf6bd6d97949447302dd7fa51415a
daria-z <daria.zakirova@gmail.com>
2017-12-05 15:22:01 +0300

    анимация формы
    форма становится невидимой, а не исчезает

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 var close = document.querySelector(".form-close");
 var modal = document.querySelector(".reservation-form")
 
-modal.classList.remove("modal-out")
+modal.classList.add("modal-out")
 close.addEventListener("click", function(){
   modal.classList.toggle("modal-out");
+})
+modal.classList.remove("modal-in")
+close.addEventListener("click", function(){
+  modal.classList.toggle("modal-in");
 })
